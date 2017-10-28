c3480594003a373e006eac317c881e956e47fa40
David Lafreniere <dlafreniere@cytori.com>
2017-10-28 08:29:25 -0700

    Add gitignore. Fix indentations.

--- Examples/WorkerThread.cpp
+++ Examples/WorkerThread.cpp
@@ -19,32 +19,32 @@ unsigned long WorkerThread::Process(void* parameter)
 	BOOL bRet;
 	while ((bRet = GetMessage(&msg, NULL, WM_USER_BEGIN, WM_USER_END)) != 0)
 	{
-switch (msg.message)
-{
-	case WM_DISPATCH_CALLBACK:
-	{
-		ASSERT_TRUE(msg.wParam != NULL);
+		switch (msg.message)
+		{
+			case WM_DISPATCH_CALLBACK:
+			{
+				ASSERT_TRUE(msg.wParam != NULL);
 
-        // Get the ThreadMsg from the wParam value
-        ThreadMsg* threadMsg = reinterpret_cast<ThreadMsg*>(msg.wParam);
+				// Get the ThreadMsg from the wParam value
+				ThreadMsg* threadMsg = reinterpret_cast<ThreadMsg*>(msg.wParam);
 
-		// Convert the ThreadMsg void* data back to a CallbackMsg* 
-		CallbackMsg* callbackMsg = static_cast<CallbackMsg*>(threadMsg->GetData()); 
+				// Convert the ThreadMsg void* data back to a CallbackMsg* 
+				CallbackMsg* callbackMsg = static_cast<CallbackMsg*>(threadMsg->GetData()); 
 
-		// Invoke the callback callback on the target thread
-		callbackMsg->GetAsyncCallback()->TargetInvoke(&callbackMsg);
+				// Invoke the callback callback on the target thread
+				callbackMsg->GetAsyncCallback()->TargetInvoke(&callbackMsg);
 
-		// Delete dynamic data passed through message queue
-		delete threadMsg;
-        break;
-	}
+				// Delete dynamic data passed through message queue
+				delete threadMsg;
+				break;
+			}
 
-	case WM_EXIT_THREAD:
-		return 0;
+			case WM_EXIT_THREAD:
+				return 0;
 
-	default:
-		ASSERT();
-}
+			default:
+				ASSERT();
+		}
 	}
 	return 0;
 }
